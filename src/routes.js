23627fac849d75f20face1bab71cda06cbcd98a9
devleal <ethan00leal@gmail.com>
2023-04-29 00:59:48 -0300

    Criei o componente Header, ajustei ele com as rotas e estilizei todo o componente

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.js';
 import Filmes from './pages/Filmes/Filmes.js';
 
+import Header from './components/Header/Header.js';
+
 function RoutesApp(){
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={ <Home/> } />
                 <Route path="/filmes/:id" element={ <Filmes/> } />  
